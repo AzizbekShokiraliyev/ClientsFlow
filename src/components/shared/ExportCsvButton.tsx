738fc54930a2b08e524602700a41e271f2c0c7f2
Azizbek Shokiraliyev <azizbekshokiraliyev03@gmail.com>
2026-06-19 15:17:13 +0500

    new

--- src/components/shared/ExportCsvButton.tsx
+++ src/components/shared/ExportCsvButton.tsx
@@ -9,11 +9,11 @@ const ExportCsvButton = ({
   keys,
 }: ExportCsvButtonProps) => {
   const handleExport = () => {
-    const rows = data.map((item) => keys.map((key) => item[key] ?? "—")) //har bir data elementi uchun keysdan tegishli qiymatlar chaqiriladi
-    const csv = [headers, ...rows].map((r) => r.join(",")).join("\n") // hadersni 1 ta qator qiladi va valuelarni vergul bilan ajratib /n yangi qatorga ajratadi
-    const blob = new Blob([csv], { type: "text/csv" }) // blob browser filega oxshash malumot yaratish type uni qanday bolishi
-    const url = URL.createObjectURL(blob) // blob uchun vaqtinchalik link yaratadi
-    const a = document.createElement("a") // bu osha fileni yuklab olish
+    const rows = data.map((item) => keys.map((key) => item[key] ?? "—"))
+    const csv = [headers, ...rows].map((r) => r.join(",")).join("\n")
+    const blob = new Blob([csv], { type: "text/csv" })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement("a")
     a.href = url
     a.download = `${filename}.csv`
     a.click()
--- src/routs/ProtectRoute.tsx
+++ src/routs/ProtectRoute.tsx
@@ -6,36 +6,28 @@ import { supabase } from "@/lib/supabase"
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation()
   const [session, setSession] = useState<Session | null | undefined>(undefined)
-  // bu yerda session bor, null yoq, undefined tikshirilmoqda
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
       setSession(data.session)
     })
-    // datsur componenti mount bolganda uni tekshiramiz
 
     const { data: listener } = supabase.auth.onAuthStateChange(
-      //listener = data = { subscription: { id, callback, unsubscribe } }
       (_event, newSession) => {
         setSession(newSession)
       }
     )
-    //u har qanday auth holati o'zgarishida ishga tushadi: login bo'lganda, logout bo'lganda ham token automatik yangilanadi
-    //auth holati o'zgarganda (login, logout, token yangilanishi va h.k.)
 
     return () => listener.subscription.unsubscribe()
-    //useEffect cleanup function agar user login qilib kirib ketgandan keyin ham biz uni toxtatmasak u ishlayveradi [] clean up unmount bolganda ishlaydi
   }, [])
 
   if (session === undefined) {
     return <div>Loading...</div>
   }
-  // user aniqlanayotgan vaqt loading boladi
 
   if (!session) {
     return <Navigate to="/login" replace state={{ from: location }} />
   }
-  //user bolmasa loginga navigate boladi
 
   return children
 }
